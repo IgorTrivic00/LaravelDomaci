77ce29693a397f1e11f65193e63b82db63cde70b
IgorTrivic00 <trivicigor00@gmail.com>
2022-12-05 12:38:23 +0100

    Dodat kod za modele koji ih povezuje sa drugim tabelama

--- KompanijaLaravel/app/Models/Zaposleni.php
+++ KompanijaLaravel/app/Models/Zaposleni.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Kompanija;
+use App\Models\Grad;
 
 class Zaposleni extends Model
 {
     use HasFactory;
+
+    public function kompanija(){
+        return $this->belongsTo(Kompanija::class);
+    }
+
+    public function grad(){
+        return $this-> belongsTo(Grad::class);
+    }
+
 }
